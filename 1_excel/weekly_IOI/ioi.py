d11432e2358cec7a5260342a60452ae23de548de
Jinha Hwang <pidokige0204@gmail.com>
2024-01-06 20:20:37 +0900

    finalize weekly IOI report creation

--- 1_excel/weekly_IOI/ioi.py
+++ 1_excel/weekly_IOI/ioi.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook, Workbook
-from openpyxl.styles import Alignment
+from openpyxl.styles import Alignment, Font
 from openpyxl.worksheet.page import PageMargins
 import pandas as pd 
 
@@ -37,16 +37,16 @@ column_name_table = {
 }
 
 product_search_table = {
-    "Osprey R4",
-    "Osprey R5",
-    "Osprey2mainline",
-    "Gemini R4",
-    "Gemini R5",
-    "Maru R4.5",
-    "Maru R4",
-    "Maru R3",
-    "Others",
-    "Advantech"
+    "Osprey R4" : 1,
+    "Osprey R5" : 2,
+    "Osprey2mainline" : 3,
+    "Gemini R4" : 4,
+    "Gemini R5" : 5,
+    "Maru R4.5" : 6,
+    "Maru R4" : 7,
+    "Maru R3" : 8,
+    "Others" : 9,
+    "Advantech" : 10
 }
 
 
@@ -67,10 +67,15 @@ def set_page_area (sheet, row_count):
 
 
 def set_cells_properties(sheet, minrow, mincol, maxcol):
+
+    bold_font = Font(bold=True)
     #set column demension with predefined one.
     for column in sheet.iter_cols(min_row=1, max_row=1, min_col=1, max_col=maxcol):
         column_letter = column[0].column_letter
         sheet.column_dimensions[column_letter].width = column_dimension_table[column_letter]
+        for cell in column:     # make title bold
+            cell.font = bold_font
+
     
     for row in sheet.iter_rows(min_row=minrow, min_col=mincol, max_col=maxcol):
         for cell in row:
@@ -132,7 +137,7 @@ def rearrange_columns(sheet):
 
 def simple_datatime(date_string):
     # 입력된 날짜를 FWXX.X format 으로 변경
-    date_object = datetime.strptime(date_string, "%m/%d/%Y")
+    date_object = datetime.strptime(date_string, "%Y-%m-%d")
 
     formatted_date = date_object.strftime("FW%W.%w") 
 
@@ -213,6 +218,24 @@ try:
     for row in report_sheet.iter_rows(min_row=1, max_row=1, values_only=True):
         new_sheet.append(row)
 
+    # sort tag column 
+    for row in report_sheet.iter_rows(min_row=2, max_row=row_count, min_col=2, max_col=2):
+        for cell in row:
+            indexlist = []
+            resultdict = {} 
+            product_string = report_sheet[cell.coordinate].value
+            result_list = product_string.split(";")
+            for x in result_list:
+                indexlist.append(product_search_table[x])   # product_search_table 와 같은 dict 를 만들기위한 1,2,3,4  를 만들기 위한 index list
+                resultdict = dict(zip(result_list, indexlist))  # product_search_table 와 같은 dict 를 만드는 과정임
+                length_of_dict = len(resultdict)
+                if(length_of_dict > 1):  # sorting 이 필요한지 확인하는 것임
+                    sorted_dict_by_value = dict(sorted(resultdict.items(), key=lambda item: item[1]))  # sorting 된 것임
+                    keys_list = list(sorted_dict_by_value.keys())  # key 값 osprey R4, osprey R5 와 같은 list 를 만드는 것암
+                    result_string = ';'.join(keys_list)  # ;으로 분리된  string (ex : Osprey R4; Osprey2mainline)
+                    report_sheet[cell.coordinate].value = result_string
+
+
     for prouct in product_search_table:
         for row in report_sheet.iter_rows(min_row=1, max_row=row_count, min_col=MIN_COL, max_col=MAX_COL, values_only=True):
             for col_num, value in enumerate(row, start=1):
@@ -222,8 +245,8 @@ try:
                     result_list = product_string.split(";")
                     # print(result_list)
                     # print(f"셀 위치: {cell_address}, 값: {value}")
-                    for x in result_list:
-                        if (x == prouct):
+                    for index, value in enumerate(result_list):
+                        if (index  == 0 and value == prouct):
                             new_sheet.append(row)
 
 

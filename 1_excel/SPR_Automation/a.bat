5fccf7c927205896fb8587512d1109c6ed26096c
Jinha Hwang <pidokige0204@gmail.com>
2024-04-13 17:02:02 +0900

    delete batch file a.bat
    delete batch file a.bat

--- 1_excel/SPR_Automation/a.bat
+++
@@ -1,4 +0,0 @@
-python .\xl_smart.py Osprey\osprey_issues_FW24.xlsx Osprey\osprey_issues_FW25.xlsx
-python .\xl_master.py Osprey\osprey_issues_FW25.xlsx
-python .\xl_analysis.py Osprey\osprey_issues_master.xlsx
-rem python .\xl_gemi2ospre.py Gemini_R4\GEMINI_R4_FW21.xlsx Gemini_R4\GEMINI_R4_FW26.xlsx
